c637a846b905b8557e13bae30b2a2dace6736dbe
Liza <elisafetka@gmail.com>
2017-11-13 12:03:03 +0300

    init lw3

---
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        FirefoxDriver firefox;
+
+        [TestMethod]
+        public void TestMethod1()
+        {              
+            firefox = new FirefoxDriver();
+            firefox.Navigate().GoToUrl("https://github.com/");
+            firefox.FindElementByXPath(".//a[text() = 'Sign in']").Click();
+            firefox.FindElementById("login_field").SendKeys("testautomationuser");
+            firefox.FindElementById("password").SendKeys("Time4Death!");
+        }
+        
+
+       
+    }
+}
